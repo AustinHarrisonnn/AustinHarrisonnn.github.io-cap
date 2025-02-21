80833a6f34ccc902f7c688234fe24b80e58e817f
Austin Harrison <austin.harrisonn@yahoo.com>
2025-02-21 13:11:47 -0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AustinHarrisonnn.github.io-cap/', 
+  base: '/AustinHarrisonnn.github.io-cap/',
 });
